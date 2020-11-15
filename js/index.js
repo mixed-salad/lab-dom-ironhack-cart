f58ce687f12e4e6e90c42d5e70deea4fb985e960
Harumi Terayama <hterayama@gmail.com>
2020-11-15 12:46:14 +0100

    all the iterations completed

--- js/index.js
+++ js/index.js
@@ -28,10 +28,12 @@ function calculateAll() {
     subtotalPricesArray.push(updateSubtotal(item));
   }
   // ITERATION 3
-  const totalPrice = subtotalPricesArray.reduce((total, price) => total + price, 0);
+  const totalPrice = subtotalPricesArray.reduce(
+    (total, price) => total + price,
+    0
+  );
   const totalValueElement = document.querySelector('#total-value span');
   totalValueElement.innerHTML = totalPrice;
-
 }
 
 // ITERATION 4
@@ -39,6 +41,11 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+  const removeElement = target.parentNode.parentNode;
+  const table = document.getElementsByTagName('tbody');
+  table[0].removeChild(removeElement);
+  calculateAll();
+
   //... your code goes here
 }
 
@@ -46,11 +53,81 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  const tableElement = document.getElementsByTagName('tbody');
+  const newProduct = document.querySelectorAll('.create-product input');
+  let newProductName = newProduct[0].value;
+  let newProductPrice = newProduct[1].value;
+  
+  const tableRowElement = document.createElement('tr');
+  tableRowElement.className = 'product';
+
+  // create new td elements 
+    //name td
+  const nameDataElement = document.createElement('td');
+  nameDataElement.className = 'name';
+  const nameValueElement = document.createElement('span');
+  nameValueElement.innerHTML = newProductName;
+  nameDataElement.appendChild(nameValueElement);
+    //price td
+  const priceDataElement = document.createElement('td');
+  const priceValueElement = document.createElement('span');
+  priceDataElement.className = 'price';
+  priceDataElement.innerHTML = '$';
+  priceValueElement.innerHTML = newProductPrice;
+  priceDataElement.appendChild(priceValueElement);
+    //quantity td
+  const quantityDataElement = document.createElement('td');
+  quantityDataElement.className = 'quantity';
+  const quantityInputelement = document.createElement('input');
+  quantityInputelement.type = 'number';
+  quantityInputelement.value = '0';
+  quantityInputelement.min = '0';
+  quantityInputelement.placeholder = 'Quantity';
+  quantityDataElement.appendChild(quantityInputelement);
+  console.log(quantityDataElement);
+    //subtotal td
+  const subtotalDataElement = document.createElement('td');
+  subtotalDataElement.className = 'subtotal';
+  const subtotalValueElement = document.createElement('span');
+  subtotalDataElement.innerHTML = '$';
+  subtotalValueElement.innerHTML = '0';
+  subtotalDataElement.appendChild(subtotalValueElement);
+    //action td
+  const actionDataElement = document.createElement('td');
+  actionDataElement.className = 'action';
+  const actionBtnElement = document.createElement('button');
+  actionBtnElement.className = 'btn btn-remove';
+  actionBtnElement.innerHTML = 'Remove';
+  actionBtnElement.addEventListener('click', removeProduct);
+  actionDataElement.appendChild(actionBtnElement);
+
+  //Appending all the td elements to tr element
+  tableRowElement.appendChild(nameDataElement);
+  tableRowElement.appendChild(priceDataElement);
+  tableRowElement.appendChild(quantityDataElement);
+  tableRowElement.appendChild(subtotalDataElement);
+  tableRowElement.appendChild(actionDataElement);
+
+  //Appending tr to tbody
+  tableElement[0].appendChild(tableRowElement);
+
+  console.log(tableRowElement);
+  //reset the values
+  newProduct[0].value = '';
+  newProduct[1].value = '0';
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const removeBtns = document.getElementsByClassName('btn-remove');
+  for (btn of removeBtns) {
+    btn.addEventListener('click', removeProduct);
+  }
+
+  const addBtn = document.getElementById('create');
+  addBtn.addEventListener('click', createProduct);
   //... your code goes here
 });
